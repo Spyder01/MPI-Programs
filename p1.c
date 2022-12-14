8b16d3f06d46f162498ca908b3d094993972d57a
Spyder01 <suhan01.bangera@gmail.com>
2022-12-14 23:41:22 +0530

    first commit

--- p1.c
+++ p1.c
@@ -2,32 +2,27 @@
 #include<string.h>
 #include<mpi.h>
 
-const int MSG_SIZE = 100;
-
 int main () {
-	char op_msg[MSG_SIZE];
-	char in_msg[MSG_SIZE];
-
+	char greetings[100];
 	int comm_sz;
-	int page_rank;
-	MPI_Status stat; 
+	int my_rank;
 
 	MPI_Init (NULL, NULL); 
 	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
-	MPI_Comm_rank(MPI_COMM_WORLD, &page_rank); 
-
-	for (int i=0; i<comm_sz; i++) {
-		if (i != page_rank) {
-			MPI_Recv(&in_msg, MSG_SIZE, MPI_CHAR, i, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
-			printf ("Recieving the follwoing message from %d of %d: %s \n", page_rank, comm_sz, in_msg);
-			
-		} else {
-		sprintf(op_msg, "Message sent from %d of %d.... \n", page_rank, comm_sz); 
-		MPI_Send(&op_msg, strlen(op_msg), MPI_CHAR, i, i, MPI_COMM_WORLD); 
-		
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
+	
+	if (my_rank !=0) {
+		sprintf(greetings, "Greeting from %d of %d\n", my_rank, comm_sz); 
+		MPI_Send(greetings, strlen(greetings)+1, MPI_CHAR, 0, 0, MPI_COMM_WORLD); 
+	} else {
+		printf ("Greeting from %d of %d\n", my_rank, comm_sz); 
+		for (int q = 1; q<comm_sz; q++) {
+			MPI_Recv (greetings, 100, MPI_CHAR, q, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+			printf("%s \n", greetings);
 		}
 	}
-	MPI_Finalize (); 
-	return 0; 
-}
+	
+	MPI_Finalize ();
 
+    	return 0;
+}
